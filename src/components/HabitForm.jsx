02fcd1de3af786a3e30569e433cae81529059e72
MargoTech <margobartashevich@gmail.com>
2025-03-21 22:52:10 +0100

    Working with HabitForm.jsx

--- src/components/HabitForm.jsx
+++ src/components/HabitForm.jsx
@@ -1,18 +1,32 @@
-<input type="text" />
+import { useState } from "react";
 
+const HabitTracker = () => {
+  const [habits, setHabits] = useState([]);
+  const [habitTitle, setHabitTitle] = useState("");
 
-
-const [habit, setHabits = useState([]);
-
-const handleAddHabit = () => {
-    const newHabit = { id: Date.now(), title: "New Habit" };
+  const handleAddHabit = () => {
+    const newHabit = { id: Date.now(), title: habitTitle };
     setHabits([...habits, newHabit]);
+    setHabitTitle("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={habitTitle}
+        onChange={(event) => setHabitTitle(event.target.value)}
+        placeholder="Add habit"
+      />
+      <button onClick={handleAddHabit}>Add Habit</button>
+
+      <ul>
+        {habits.map((habit) => (
+          <li key={habit.id}>{habit.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-<button onClick={handleAddHabit}>Add Habit</button>
-
-<ul>
-  {habits.map((habit) => (
-    <li key={habit.id}>{habit.title}</li>
-  ))}
-</ul>
+export default HabitTracker;
