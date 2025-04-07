b6b9b9a108404e466fe0d2b28032bad8844cf84a
MargoTech <margobartashevich@gmail.com>
2025-04-07 23:06:27 +0200

    Working with firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA6EalGxYRld8cOi9o3GJy5yebM9aMigo8",
   authDomain: "habit-tracker-2c04f.firebaseapp.com",
   projectId: "habit-tracker-2c04f",
-  storageBucket: "habit-tracker-2c04f.firebasestorage.app",
+  storageBucket: "habit-tracker-2c04f.appspot.com",
   messagingSenderId: "190637713455",
   appId: "1:190637713455:web:a7e5447873defac4dbbeda",
 };
